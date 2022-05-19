2cd8e457a23fb549a8e56fefc7c240a1041d1b22
James Totah <jtotah@usfca.edu>
2022-05-19 15:52:32 -0500

    UPDATE: added command line scanner and file scanners, with new debug sysouts

--- Main.java
+++ Main.java
@@ -1,12 +1,55 @@
+import java.util.Scanner;
+import java.io.File; 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 class Main{
     public static void main(String[] args){
-        Driver theodore = new Driver("Thomas Drummond");
 
-        Address dest1 = new Address("1234 Harmon Way, Oklahoma City, OK, 73000");
+        Scanner fileInputs = new Scanner(System.in);
 
-        Match match1 = new Match(theodore, dest1);
+        System.out.println("Enter the driver names filepath:");
+        String driverFilePath = fileInputs.nextLine();
+        System.out.println("Enter the destinations filepath:");
+        String destinationFilePath = fileInputs.nextLine();
 
-        System.out.println(match1.toString());
+        fileInputs.close();
+
+        ArrayList<Driver> drivers = new ArrayList<Driver>();
+        ArrayList<Address> destinations = new ArrayList<Address>();
+
+        try{
+            File driverFile = new File(driverFilePath);
+            Scanner lineReader = new Scanner(driverFile);
+            while(lineReader.hasNextLine()){
+                drivers.add(new Driver(lineReader.nextLine()));
+            }
+            lineReader.close();
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        try{
+            File destinationsFile = new File(destinationFilePath);
+            Scanner lineReader = new Scanner(destinationsFile);
+            while(lineReader.hasNextLine()){
+                destinations.add(new Address(lineReader.nextLine()));
+            }
+            lineReader.close();
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+       
+        System.out.println(drivers);
+        System.out.println("");
+        System.out.println(destinations);
+
+        // Driver theodore = new Driver("Thomas Drummond");
+
+        // Address dest1 = new Address("1234 Harmon Way, Oklahoma City, OK, 73000");
+
+        // Match match1 = new Match(theodore, dest1);
+
+        // System.out.println(match1.toString());
     }
 }
