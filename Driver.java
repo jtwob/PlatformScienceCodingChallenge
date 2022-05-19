64cf4a78d052ce8d3431fbf3b3933b43cce11152
James Totah <jtotah@usfca.edu>
2022-05-19 14:11:06 -0500

    NEW: added Main file, Driver, Address, and Match classes, Driver class preliminarily complete

---
+++ Driver.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+
+/**
+ * Driver Class
+ */
+public class Driver {
+    String name;
+    int length;
+    ArrayList<Integer> factors;
+    double evenSS;
+    double oddSS;
+
+    /**
+     * The Driver Object constructor, takes name of a driver and generates 
+     * the rest of the class variables data from the name. 
+     * @param n
+     */
+    public Driver(String n){
+        this.name = n;
+        this.length = name.length();
+        this.factors = new ArrayList<Integer>();
+        factorFinder();
+        this.evenSS = numVowels()*1.5;
+        this.oddSS = numConsonants();
+    }
+
+    /**
+     * factorFinder function uses the predetermined length of the Driver's name
+     * to generate the list of factors needed for the SS multiplier. Called in
+     * the Driver constructor immediately after factors variable is initialized. 
+     */
+    private void factorFinder(){
+        for(int i = 2; i < this.length; i++){
+            if(this.length%i == 0){
+                this.factors.add(i);
+            }
+        }
+    }
+
+    /**
+     * numVowels uses class variables to determine the number of vowels in
+     * the provided driver name.
+     * @return the number of vowels
+     */
+    private int numVowels(){
+        int count = 0;
+        for(int i = 0; i < this.length; i++){
+            switch(this.name.charAt(i)){
+                case 'a': count++;
+                break;
+                case 'e': count++;
+                break;
+                case 'i': count++;
+                break;
+                case 'o': count++;
+                break;
+                case 'u': count++;
+                break;
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * numConsonants uses class variables to determine the number of consonants
+     * (not including whitespace) in the provided driver name.
+     * @return the number of consonants
+     */
+    private int numConsonants(){
+        int count = this.length;
+        for(int i = 0; i < this.length; i++){
+            switch(this.name.charAt(i)){
+                case 'a': count--;
+                break;
+                case 'e': count--;
+                break;
+                case 'i': count--;
+                break;
+                case 'o': count--;
+                break;
+                case 'u': count--;
+                break;
+                case ' ': count--;
+                break;
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * Overrided toString for testing and general use.
+     * @return String of Driver data
+     */
+    public String toString(){
+        return ("Name: " + this.name + "\nFactors: " + this.factors +"\nEvenSS: "+ this.evenSS+"\nOddSS: "+ this.oddSS);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+
+class Main{
+    public static void main(String[] args){
+        Driver theodore = new Driver("Theodore Browne");
+        System.out.println(theodore.toString());
+
+    }
+}
